030bc9b7321a2b59065d33c5dea26ac6c72dccdf
pedro-herrera-r <pedro.herrera@rocketbot.com>
2025-01-09 17:13:45 -0300

    feat: execution python, node, cli

---
+++ __init__.py
@@ -0,0 +1,97 @@
+import subprocess
+import sys, os
+
+GetParams = GetParams # type: ignore
+SetVar = SetVar # type: ignore
+PrintException = PrintException # type: ignore
+
+base_path = tmp_global_obj["basepath"] # type: ignore
+cur_path = base_path + 'modules' + os.sep + 'ScriptsAdvanced' + os.sep + 'libs' + os.sep
+
+
+if cur_path not in sys.path:
+    sys.path.append(cur_path)
+
+module = GetParams("module")
+
+try:
+
+    if module == "python":
+        try:
+            file = GetParams("file")
+            result_studio = GetParams("result")
+            args = list(GetParams("arguments"))
+            print(args, type(args))
+
+            if not file.endswith(".py"):
+                raise Exception("No python files are not supported for python interpreter")
+
+            parameters = ['python', file]
+            parameters.extend(args)
+            print(parameters)
+
+            result = subprocess.run(parameters, check=False, capture_output=True, text=True)
+            
+            if "CommandNotFoundException" in result.stderr:
+                raise Exception("Python not found locally, please install it and add to the path following this link:\nhttps://realpython.com/add-python-to-path/")
+
+            if result.returncode != 0:
+                raise Exception(result.stderr)
+
+            SetVar(result_studio, result.stdout)
+
+        except Exception as e:
+            PrintException()
+            raise e
+        
+    if module == "node":
+        try:
+            file = GetParams("file")
+            result_studio = GetParams("result")
+
+            if not file.endswith(".js"):
+                raise Exception("No Node files are not supported for Node interpreter")
+
+            result = subprocess.run(['node', file], check=False, capture_output=True, text=True)
+
+            if "CommandNotFoundException" in result.stderr:
+                raise Exception("Node not found locally, please install it following this link:\nhttps://nodejs.org/")
+
+            if result.returncode != 0:
+                raise result.stderr
+            
+            SetVar(result_studio, result.stdout)
+
+        except Exception as e:
+            PrintException()
+            raise e
+        
+    if module == "cli":
+        instructions = GetParams("instructions").split('\n')
+        result_studio = GetParams("result")
+        output = []
+        errors = []
+
+        try:
+            for instruction in instructions:
+
+                result = subprocess.run(instruction, shell=True, capture_output=True, text=True)
+
+                if result.returncode != 0:
+                    errors.append(result.stderr.strip())
+                
+                output.append(result.stdout)
+            if errors:
+                raise Exception('\n'.join(errors))
+
+            output = [item.strip() for item in output]
+
+            SetVar(result_studio, output)
+
+        except Exception as e:
+            PrintException()
+            raise e
+
+except Exception as e:
+    PrintException()
+    raise e
